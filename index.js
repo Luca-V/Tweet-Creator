e52c9f4d41ea10e371e6a1bd040093cdc6b2ea97
DaddyRebotingg <87208955+DaddyRebotingg@users.noreply.github.com>
2022-01-16 22:49:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const { Twitter } = require("twitter");
+
+const client = new Twitter({
+    consumer_key: "",
+    consumer_secret: "",
+    access_token_key: "",
+    access_token_secret: ""
+});
+
+const params = {
+    // screen name of the user
+    q: "",
+
+    // number of tweets to return
+    count: 10,
+};
+
+// lookup tweets of the user twitter account
+client.get("search/tweets", params, function (error, tweets, response) {
+    console.log(tweets);
+});
+
+// post a tweet
+client.post("statuses/update", { status: "" }, function (error, tweet, response) {
+    console.log(tweet);
+});
